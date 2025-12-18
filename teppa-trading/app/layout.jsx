95b7723325d303f8e8eee1fe25ac7ca8edf84e70
teppatrading-collab <teppatrading@gmail.com>
2025-12-18 01:16:20 -0500

    Update layout.jsx

--- teppa-trading/app/layout.jsx
+++ teppa-trading/app/layout.jsx
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Teppa Trading",
+  description: "Free and Paid Trading Education by Teppa Trading",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
